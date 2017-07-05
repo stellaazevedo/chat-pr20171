5e30d56623feb4d9889bbd28abdf939bdfbcf190
rezembrim <rezembrim.soares@gmail.com>
2017-07-05 19:59:50 -0300

    Envio do arquivo base app-chat.py. Commit Inicial

---
+++ app-chat.py
@@ -0,0 +1,52 @@
+'''
+Protocolo de aplicação - Chat
+Requisitos
+1 - Conexão numa sala de bate papo;
+2 - Comando para entrar e sair (join e quit)
+3 - Servidor armazena o IP dos clientes (json);
+4 - Todos enviam e recebem mensagens;
+5 - Clientes ilimitados.
+6 - Vincular nome a IP
+
+Requisitos (05/07/2017)
+-Não funcionais:
+    -Código no github
+    -Link do código no classroom
+    -Integração da aplicação com pelo menos 1 grupo na sala
+-Funcionais
+    -Cliente e servidor tcp ou udp
+    -3 funcionalidades mínimas no protocolo (join+username, listusers, sendwhisper)
+    -Desconexão do cliente ao enviar o comando '#quit' ao servidor
+    -Sistema de log (json ou txt) com todas as conexões e dados recebidos
+'''
+
+#!/usr/bin/env python
+
+import socket
+
+def Start(arg):
+    pass
+
+def Join(arg):
+    pass
+
+def Quit(arg):
+    pass
+
+def ListUsers(arg):
+    pass
+
+def SendAll(arg):
+    pass
+
+def SendWhisper(arg):
+    pass
+
+def SendList(arg):
+    pass
+
+def Ok(arg):
+    pass
+
+if __name__ == '__main__':
+    Start()
